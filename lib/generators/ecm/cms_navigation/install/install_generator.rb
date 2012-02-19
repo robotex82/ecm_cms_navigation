69d6e2e20abaaa48a3117429a3856ca848420fc7
Roberto Vasquez Angel <robotex@robotex.de>
2012-02-19 23:00:40 +0100

    Fixed install generator.

--- lib/generators/ecm/cms_navigation/install/install_generator.rb
+++ lib/generators/ecm/cms_navigation/install/install_generator.rb
@@ -7,19 +7,19 @@ module Ecm
         desc "Installs the ECM CMS Navigation module"
 
         def generate_active_admin
-          generate 'ecm:cms_core:active_admin'
+          generate 'ecm:cms_navigation:active_admin'
         end              
         
         def generate_locales
-          generate 'ecm:cms_core:locales'
+          generate 'ecm:cms_navigation:locales'
         end
         
         def generate_migrations
-          generate 'ecm:cms_core:migrations'
+          generate 'ecm:cms_navigation:migrations'
         end
         
         def generate_models
-          generate 'ecm:cms_core:models'
+          generate 'ecm:cms_navigation:models'
         end
       end
     end
